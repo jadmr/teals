23a696db673b460f89f89ef4e4822ae23995a3eb
JAD <programming@coffeemark.us>
2022-03-18 21:04:33 -0700

    Refactor how addition is checked; penalize addition error more

--- hanford/ap_csa/exams/two_d_arrays/frq_2/grading/AdditionEvaluator.java
+++ hanford/ap_csa/exams/two_d_arrays/frq_2/grading/AdditionEvaluator.java
@@ -6,21 +6,21 @@ public class AdditionEvaluator extends Evaluator {
 
     @Override
     protected void EvaluateAnswer(Answer answer, Score score) {
-        answer.brighten(this.pixels, 10);
+        answer.brighten(this.pixels, 5);
 
         boolean didNotAddCorrectly = false;
 
         for (int row = 0; row < pixels.length; row++) {
             for (int col = 0; col < pixels.length; col++) {
-                if (pixels[row][col] != 110) {
+                if (pixels[row][col] != 105) {
                     didNotAddCorrectly = true;
                 }
             }
         }
 
         if (didNotAddCorrectly) {
-            score.decrementGradeAsPercentage(10);
-            score.addError("pixel values were not incremented correctly", 10);
+            score.decrementGradeAsPercentage(25);
+            score.addError("pixel values were not incremented correctly", 25);
         }
     }
 }

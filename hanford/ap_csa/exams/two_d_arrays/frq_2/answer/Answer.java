a48bbb632990af94f649f696ef64f81090882dc0
JAD <programming@coffeemark.us>
2022-03-21 21:25:14 -0700

    Add flag indicating submitter did not attempt to solve the problem

--- hanford/ap_csa/exams/two_d_arrays/frq_2/answer/Answer.java
+++ hanford/ap_csa/exams/two_d_arrays/frq_2/answer/Answer.java
@@ -3,10 +3,12 @@ package answer;
 public abstract class Answer {
     protected int id;
     protected boolean didCompile;
+    protected boolean didAttemptAnswer;
 
     protected Answer(int id) {
         this.id = id;
         this.didCompile = true;
+        this.didAttemptAnswer = true;
     }
 
     public abstract void brighten(int[][] pixels, int amount);
@@ -19,7 +21,15 @@ public abstract class Answer {
         this.didCompile = false;
     }
 
+    public void flagAnswerAsNotAttempted() {
+        this.didAttemptAnswer = false;
+    }
+
     public boolean getDidCompile() {
         return this.didCompile;
+    }
+
+    public boolean getDidAttemptAnswer() {
+        return this.didAttemptAnswer;
     }
 }
